dab4c20e70804fc35327485bbe4a21669049d431
ant9240 <anthonyllorente09@gmail.com>
2024-04-26 21:27:03 -0400

    Cambio de Canal de Youtube por Ni Me Ladilles, Hice el cambio del API. Se hizo fetch de los datos y se llenaron en el HTML

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,12 +1,12 @@
-const API = 'https://youtube138.p.rapidapi.com/channel/videos/?id=UC55-mxUj5Nj3niXFReG44OQ&filter=videos_latest&hl=en&gl=US';
+const API = 'https://youtube-v2.p.rapidapi.com/channel/videos?channel_id=UCFOdyXFDmAHyuhq042HJaOg';
 
 const content = document.getElementById('content');
 
 const options = {
     method: 'GET',
-	headers: {
-        'X-RapidAPI-Key': '7dbd182706msh630c29308d00aa5p18a517jsn7e891c82103a',
-		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+	  headers: {
+      'X-RapidAPI-Key': '7dbd182706msh630c29308d00aa5p18a517jsn7e891c82103a',
+      'X-RapidAPI-Host': 'youtube-v2.p.rapidapi.com'
 	}
 };
 
@@ -21,24 +21,25 @@ async function fetchData(urlApi) {
         const videos = await fetchData(API);
         let htmlView = '';
         for (let i = 0; i < 12; i++) {
-            let element = videos.contents[i];
-            const img = element.video.thumbnails[3].url;
-            const title = element.video.title;
-            let view = `
-            <div class="group relative">
-              <div
-                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                <img src="${img}" alt="${title}" class="w-full">
+          const element = videos.videos[i]
+          console.log(element);
+          const title = element.title
+          const img = element.thumbnails[3].url
+          const link = element
+              let view = `
+              <div class="group relative">
+                <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                  <img src="${img}" alt="${title}" class="w-full">
+                </div>
+                <div class="mt-4 flex justify-between">
+                  <h3 class="text-sm text-gray-700">
+                    <span aria-hidden="true" class="absolute inset-0"></span>
+                    ${title}
+                  </h3>
+                </div>
               </div>
-              <div class="mt-4 flex justify-between">
-                <h3 class="text-sm text-gray-700">
-                  <span aria-hidden="true" class="absolute inset-0"></span>
-                  ${title}
-                </h3>
-              </div>
-            </div>
-            `;
-            htmlView += view
+              `;
+              htmlView += view
         }
         content.innerHTML = htmlView
     } catch (error) {
